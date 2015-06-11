81cbe29172af8778d174d0546c3fcee947a579f5
VladimirTsukanov <tsv@ciklum.com>
2015-06-11 23:27:48 +0300

    project was setuped

---
+++ src/main/java/ua/startup/smartcounter/MainActivity.java
@@ -0,0 +1,19 @@
+package ua.startup.smartcounter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_layout);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+}
